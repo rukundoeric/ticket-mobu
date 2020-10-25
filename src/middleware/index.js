fdd64e82ba611fea20b75f6555dc14e9cafb4f42
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-10-25 15:38:52 +0200

    Add validation

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,3 +1,5 @@
 import glabalMiddleware from "./_global_middle_ware";
+import { verifyToken } from "./_auth";
+import validator from "./_validator";
 
-export { glabalMiddleware };
+export { glabalMiddleware, verifyToken, validator };
