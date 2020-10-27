0e2d7d71bfb45d4d397f76fe59fc43580318af75
Urimubenshi Daniel <benshidanny11@gmail.com>
2020-10-27 08:30:33 +0200

    Adde get list of all agencies feature

--- src/api/controllers/AgencyControler.js
+++ src/api/controllers/AgencyControler.js
@@ -4,7 +4,14 @@ import models from "../../sequelize/models";
 dotenv.config();
 
 const { agency } = models;
+
 class Agency {
+  /**
+   * creating a new user
+   * @param {object} req - Request.
+   * @param {object} res - Response.
+   * @returns {object} - returns created user
+   */
   static async createAgency(req, res) {
     const newAgency = {
       agencyName: req.body.agencyName,
@@ -17,13 +24,22 @@ class Agency {
     const inserter = await agency.create(newAgency);
     const agencyRes = inserter.dataValues;
 
-    res.status(201).json({
+   return res.status(201).json({
       status: 201,
       data: agencyRes,
     });
   }
 
-  static async getAllAgencies() {}
+  static async getAllAgencies(req, res) {
+    const agencyResult = await agency.findAll();
+
+    const data = agencyResult.map((item) => item.dataValues);
+
+    return res.status(200).json({
+      status: 200,
+      agencies:data,
+    });
+  }
 }
 
 export default Agency;
